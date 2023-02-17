d21fc1182f78735f1f139a5ffe9b512116c6faab
Vasily Pozdnyakov <vasily.pozdnyakov@tngtech.com>
2023-02-17 14:34:15 +0100

    Changes 1

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Camera, CameraType } from 'expo-camera';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>This is my test app</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
