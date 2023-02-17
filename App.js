f4c38c69d093dcc9147802f9fb0656125ec5178f
Vasily Pozdnyakov <vasily.pozdnyakov@tngtech.com>
2023-02-17 14:28:26 +0100

    Some changes

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is my test app</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffa',
     alignItems: 'center',
     justifyContent: 'center',
   },
